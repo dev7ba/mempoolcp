c3d149d52219066e14ec02ae1b0c107541459216
dev7ba <dev7ba@protonmail.com>
2023-02-07 18:23:08 +0100

    Cmd line parsing and validating

---
+++ src/config.rs
@@ -0,0 +1,166 @@
+extern crate serde;
+
+use std::path::PathBuf;
+
+use clap::{Parser, ValueEnum};
+use serde::{Deserialize, Serialize};
+
+use anyhow::Result;
+use std::fmt;
+
+#[derive(Debug, Serialize, Deserialize, Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+pub struct Config {
+    ///Source bitcoind node rpc url i.e. http://localhost, 's' for source_url defined in config
+    ///file
+    #[arg(group = "source")]
+    pub source_ip_addr: String,
+    ///Destination bitcoind node rpc url i.e. http://localhost, 'd' for dest_url defined in config
+    ///file
+    #[arg(group = "dest")]
+    pub dest_ip_addr: String,
+
+    ///User name for source bitcoin node
+    #[arg(short = 'n', long, requires = "source")]
+    pub source_user: Option<String>,
+    ///Password for source bitcoin node
+    #[arg(short = 'w', long, requires = "source")]
+    pub source_passwd: Option<String>,
+    ///Port for source bitcoin node rpc, use only to override --net network defaults
+    #[arg(short = 'p', long, requires = "source")]
+    pub source_port: Option<u16>,
+    ///User name for destination bitcoin node
+    #[arg(short = 'N', long, requires = "dest")]
+    pub dest_user: Option<String>,
+    ///Password for destination bitcoin node
+    #[arg(short = 'W', long, requires = "dest")]
+    pub dest_passwd: Option<String>,
+    ///Port for destination bitcoin node rpc, use only to override --net network defaults
+    #[arg(short = 'P', long, requires = "dest")]
+    pub dest_port: Option<u16>,
+    ///Bitcoin network type. Sets rpc port default.
+    #[arg(short = 't',long,default_value_t=Net::MainNet, value_enum)]
+    pub net: Net,
+    ///Use get_raw_mempool_verbose rpc call which is faster but consumes a lot of mememory.
+    #[arg(short, long, default_value_t = false)]
+    pub fast_mode: bool,
+    ///Show effective configuration
+    #[arg(short, long)]
+    pub verbose: bool,
+
+    ///Use config in ~/.config/default-config.toml If file do not exists create it with current params
+    #[arg(short = 'c', long, group = "file")]
+    #[serde(skip)]
+    use_config: bool,
+    ///Use config in path i.e. ./config.toml If file do not exists create it with current params
+    #[arg(short = 'C', long, group = "file")]
+    #[serde(skip)]
+    use_config_path: Option<PathBuf>,
+}
+
+impl fmt::Display for Config {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            r#"
+Effective configuration: 
+    Source ip: {}
+    Source port: {:?}
+    Source user name: ****
+    Source password: ****
+    Destination ip: {}
+    Destination port: {:?}
+    Destination user name: ****
+    Destination password: ****
+    Network: {:?}
+    Fast Mode: {:?}
+    Verbose: {:?}
+    Config file used: {:?}
+            "#,
+            &self.source_ip_addr,
+            &self.source_port.unwrap(),
+            &self.dest_ip_addr,
+            &self.dest_port.unwrap(),
+            &self.net,
+            &self.fast_mode,
+            &self.verbose,
+            &self.config_file_used(),
+        )
+    }
+}
+
+impl std::default::Default for Config {
+    fn default() -> Self {
+        Self {
+            source_ip_addr: "127.0.0.1".to_string(),
+            source_user: None,
+            source_passwd: None,
+            source_port: None,
+            dest_ip_addr: "127.0.0.1".to_string(),
+            dest_user: None,
+            dest_passwd: None,
+            dest_port: None,
+            net: Net::MainNet,
+            fast_mode: false,
+            use_config: false,
+            use_config_path: None,
+            verbose: false,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize, ValueEnum)]
+pub enum Net {
+    MainNet = 8332,
+    TestNet = 18332,
+    SigNet = 38332,
+    RegTest = 18443,
+}
+
+impl Config {
+    pub fn load() -> Result<Self> {
+        let mut cfg = Config::parse();
+        if cfg.use_config {
+            cfg = confy::load("mempoolcp", None)?;
+            cfg.use_config = true;
+        }
+        if cfg.use_config_path.is_some() {
+            let mut config: Config =
+                confy::load("mempoolcp", cfg.use_config_path.as_ref().unwrap().to_str())?;
+            config.use_config_path = cfg.use_config_path;
+            cfg = config;
+        }
+        if cfg.source_user.is_none() {
+            cfg.source_user = rpassword::prompt_password("Source bitcoind node user: ").ok();
+        }
+        if cfg.source_passwd.is_none() {
+            cfg.source_passwd = rpassword::prompt_password("Source bitcoind node password: ").ok();
+        }
+        if cfg.dest_user.is_none() {
+            cfg.dest_user = rpassword::prompt_password("Destination bitcoind node user: ").ok();
+        }
+        if cfg.dest_passwd.is_none() {
+            cfg.dest_passwd =
+                rpassword::prompt_password("Destination bitcoind node password: ").ok();
+        }
+        cfg.source_port = Some(cfg.source_port.unwrap_or(cfg.net as u16));
+        cfg.dest_port = Some(cfg.dest_port.unwrap_or(cfg.net as u16));
+
+        Ok(cfg)
+    }
+    fn config_file_used(&self) -> String {
+        if self.use_config {
+            "~/.config/default_config".to_string()
+        } else if self.use_config_path.is_some() {
+            self.use_config_path
+                .as_ref()
+                .unwrap()
+                .to_str()
+                .unwrap()
+                .to_string()
+        } else {
+            "None".to_string()
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,101 @@
 extern crate bitcoincore_rpc;
+extern crate confy;
 
+// use bitcoincore_rpc::jsonrpc::client;
+// use bitcoincore_rpc::{bitcoin::Txid, Auth, Client, RpcApi};
 use bitcoincore_rpc::{Auth, Client, RpcApi};
 
-fn main() {
+use anyhow::Context;
+use anyhow::Result;
+use config::Config;
+
+mod config;
+
+fn main() -> Result<()> {
+    let cfg = Config::load().with_context(|| "Error loading configuration".to_string())?;
+    if cfg.verbose {
+        println!("{}", cfg);
+    }
+
+    let source_rpc = get_client(
+        &cfg.source_ip_addr,
+        cfg.source_user.unwrap(),
+        cfg.source_passwd.unwrap(),
+        ClientType::Source,
+    )?;
+    let dest_rpc = get_client(
+        &cfg.dest_ip_addr,
+        cfg.dest_user.unwrap(),
+        cfg.dest_passwd.unwrap(),
+        ClientType::Destination,
+    )?;
+
+    let source_get_mempool_info_result = source_rpc
+        .get_mempool_info()
+        .with_context(|| format!("Can't connect to {}", cfg.source_ip_addr))?;
+    let dest_get_mempool_info_result = dest_rpc
+        .get_mempool_info()
+        .with_context(|| format!("Can't connect to {}", cfg.source_ip_addr))?;
+    if cfg.verbose {
+        println!(
+            "# Transactions in source mempool/destination mempool: {}/{}",
+            source_get_mempool_info_result.size, dest_get_mempool_info_result.size
+        );
+    }
+    Ok(())
+
+    //#[derive(Debug)]
+    // struct TxDepth {
+    //     ancestor_count: u64,
+    //     tx_id: Txid,
+    // }
+    /*
     let rpc = Client::new(
-        "http://localhost:8332",
-        Auth::UserPass("anon".to_string(), "anon".to_string()),
+        &cfg.source_url,
+        Auth::UserPass(cfg.source_user, cfg.source_passwd),
     )
-    .unwrap();
-    let best_block_hash = rpc.get_best_block_hash().unwrap();
-    println!("best block hash: {}", best_block_hash);
+    .with_context(|| format!("Can't connect to {}", cfg.source_url))?;
 
-    // let tx_id: Txid =
-    //     Txid::from_str("72951d0e0b87cb9d8b82325523e963ed6d67c931d208e8d74d6f12131c6c5d87").unwrap();
-    // let get_mempool_entry_result = rpc.get_mempool_entry(&tx_id).unwrap();
-    // println!("{:?}", get_mempool_entry_result);
+    let get_mempool_info_result = rpc
+        .get_mempool_info()
+        .with_context(|| format!("Can't connect to {}", cfg.source_url))?;
+    println!("# Transactions in mepool: {}", get_mempool_info_result.size);
 
-    let get_mempool_info_result = rpc.get_mempool_info().unwrap();
-    println!("{:?}", get_mempool_info_result);
+    let vec: Vec<TxDepth> = rpc
+        .get_raw_mempool_verbose()?
+        .iter()
+        .filter_map(|(tx_ide, mempool_entry)| {
+            if mempool_entry.ancestor_count == 1 {
+                // rpc.
+            }
+            // Next rpc call can fail if txIde has been removed from mempool while executing this
+            // program.
+            match rpc.get_raw_transaction_hex(tx_ide, None).ok() {
+                None => None,
+                Some(tx_raw) => Some(TxDepth {
+                    // raw_tx_hex: tx_raw,
+                    ancestor_count: mempool_entry.ancestor_count,
+                    tx_id: tx_ide.clone(),
+                }),
+            }
+        })
+        .collect();
 
-    //
-    //Txid::from_str(
-    // "72951d0e0b87cb9d8b82325523e963ed6d67c931d208e8d74d6f12131c6c5d87",
-    // ));
+    // println!("{:#?}", vec);
+    */
+}
+#[derive(Debug)]
+enum ClientType {
+    Source,
+    Destination,
+}
+
+fn get_client(
+    ip: &str,
+    user_name: String,
+    passwd: String,
+    client_type: ClientType,
+) -> Result<Client> {
+    Client::new(ip, Auth::UserPass(user_name, passwd))
+        .with_context(|| format!("Can't connect to {:?} bitcoind node: {}", client_type, ip))
 }
